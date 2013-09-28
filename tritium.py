9ce40dd4cfad6522fa05b507df95e538efe78b43
Aaron Miller <aaron.miller04@gmail.com>
2013-09-27 21:03:04 -0700

    still trying to get this to work (_tuple is not defined, wtf)

--- tritium.py
+++ tritium.py
@@ -9,10 +9,37 @@ from collections import namedtuple
 
 charset_base3 = 'T01'    # represented by 0b11, 0b00, and 0b01, respectively; 0b10 is invalid
 
+trit_to_bit  = {'T': 0b11,
+                '0': 0b00,
+                '1': 0b01}
+
 charset_base27 = 'MNPQRSTUVWXYZ0123456789ABCD'  # I left out 'O' because it looks too much like '0'
 
 class TritNumber(namedtuple('TritNumber', 'n checked')):   # n is a non-zero integer (really a bitmap); checked is boolean
     __slots__ = ()
+
+    def __new__(_cls, *args, **keys):
+        if len(keys) != 0 or len(args) > 2:
+            raise ValueError('expected either a string or (integer-bitmap, boolean) as arguments')
+        if len(args) == 2:
+            return _tuple.__new__(_cls, tuple(args))    # this is the standard named tuple behavior
+        # now, we are processing a string that matches /\*?(0g)?[T01]+/
+        s = args[0]
+        n = 0
+        if type(s) is not str and type(s) is not unicode:
+            self.__init__(self, *args, **keys)  # pass to the super-class (a named tuple)
+            return
+        if s.startswith('*'):
+            s = s[1:]
+        if s.startswith('0g'):
+            s = s[2:]
+        if len(s) == 0:
+            raise ValueError('no digits in balanced trinary number %r' % s)
+        for digit in s:
+            if not trit_to_bit.has_key(digit):
+                raise ValueError('invalid trinary digit %r' % digit)
+            n = (n << 2) | trit_to_bit[digit]
+        return _tuple.__new__(_cls, (n, True))
 
     def __str__(self):
         s = '0g'
